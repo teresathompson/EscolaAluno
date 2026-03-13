c1088fe1e07efca35928956a7d41ea8703bb5584
Teresa Thompson <teresaanne0101@gmail.com>
2026-03-13 17:28:07 -0300

    adiciona comentários explicativos na classe Aluno e na classe Apresentacao

--- src/ExercicioUnidade3Aula3/Aluno.java
+++ src/ExercicioUnidade3Aula3/Aluno.java
@@ -3,6 +3,8 @@ package ExercicioUnidade3Aula3;
 // Aluno é uma subclasse de Pessoa, herda de Pessoa e implementa o método abstrato apresentar()
 public class Aluno extends Pessoa {
 
+    // Atributo específico da classe Aluno
+
     private int matricula;
 
     public Aluno(String nome, int matricula) {
@@ -10,8 +12,11 @@ public class Aluno extends Pessoa {
         this.matricula = matricula;
     }
 
+    // Sobrescreve o método apresentar() da classe Pessoa para fornecer uma
+    // implementação específica para Aluno
     @Override
     public void apresentar() {
+        // Exibe as informações do aluno, incluindo o nome e a matrícula
         System.out.println("Olá, eu sou o(a) aluno(a) " + nome + " e minha matrícula é " + matricula + "." + "\n");
     }
 }
--- src/ExercicioUnidade3Aula3/Apresentacao.java
+++ src/ExercicioUnidade3Aula3/Apresentacao.java
@@ -2,6 +2,10 @@ package ExercicioUnidade3Aula3;
 
 public class Apresentacao {
     public static void main(String[] args) {
+
+        // Exibe uma mensagem de apresentação para o aluno utilizando herança e
+        // polimorfismo
+
         System.out.println("Apresentação de Aluno utilizando herança e polimorfismo:\n");
 
         System.out.println("           __________________");
@@ -16,7 +20,10 @@ public class Apresentacao {
         System.out.println("        |       |____|       |");
         System.out.println("        |____________________|" + "\n");
 
+        // Criando um objeto do tipo Aluno e chamando o método apresentar() para exibir
+        // as informações do aluno
         Aluno aluno1 = new Aluno("Lais", 1234);
+        // Chamando o método apresentar() para exibir as informações do aluno
         aluno1.apresentar();
     }
 
